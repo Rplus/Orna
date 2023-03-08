73502408cabbf53c571cbf582846cf230f454e39
Rplus <rplus.tw@gmail.com>
2023-03-08 17:15:35 +0800

    fix deploy path

--- map/vite.config.js
+++ map/vite.config.js
@@ -4,5 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Orna/map/',
+  build: {
+    outDir: '../dist/map'
+  },
   plugins: [svelte()]
 })
